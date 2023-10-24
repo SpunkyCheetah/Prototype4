28b1ee5d6c48b673605fb6bc5fc885fab890d118
SpunkyCheetah <Iliana.Bernadette@gmail.com>
2023-10-24 13:26:52 -0700

    4.2 completed

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -17,6 +17,6 @@ public class RotateCamera : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime * -1);
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
     }
 }
